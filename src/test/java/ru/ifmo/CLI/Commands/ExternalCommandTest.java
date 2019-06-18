e64d3568362eeab557a7f57ecb36f2040f6d2f2f
Vladislav Platonov <v.platonov@phoenixcas.com>
2019-06-18 12:41:42 +0300

    Use fileSystem separators in ExternalCommandTest

--- src/test/java/ru/ifmo/CLI/Commands/ExternalCommandTest.java
+++ src/test/java/ru/ifmo/CLI/Commands/ExternalCommandTest.java
@@ -3,6 +3,7 @@ package ru.ifmo.CLI.Commands;
 import org.junit.jupiter.api.Test;
 import ru.ifmo.CLI.Utils.IOData;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,7 +15,12 @@ public class ExternalCommandTest {
 
     @Test
     public void testCallExternalWithArguments() {
-        List<String> arguments = Stream.of("src\\test\\resources\\Summator\\Debug\\Summator",
+        List<String> arguments = Stream.of("src" + File.separator +
+                        "test" + File.separator +
+                        "resources" + File.separator +
+                        "Summator" + File.separator +
+                        "Debug" + File.separator +
+                        "Summator",
                 String.valueOf(5), String.valueOf(7)).collect(Collectors.toList());
         Command command = new ExternalCommand();
         command.addArguments(arguments);
@@ -26,13 +32,21 @@ public class ExternalCommandTest {
     @Test
     public void testCallExternalFromPipe() {
         List<String> catArguments = new ArrayList<>();
-        catArguments.add("src\\test\\resources\\summator input.txt");
+        catArguments.add("src" + File.separator +
+                        "test" + File.separator +
+                        "resources" + File.separator +
+                        "summator input.txt");
         Command catCommand = new CatCommand();
         catCommand.addArguments(catArguments);
         IOData catResult = catCommand.execute();
         Command externalCommand = new ExternalCommand();
         List<String> externalArguments = new ArrayList<>();
-        externalArguments.add("src\\test\\resources\\Summator\\Debug\\Summator");
+        externalArguments.add("src" + File.separator +
+                        "test" + File.separator +
+                        "resources" + File.separator +
+                        "Summator" + File.separator +
+                        "Debug" + File.separator +
+                        "Summator");
         externalCommand.addArguments(externalArguments);
         externalCommand.setIOData(catResult);
         IOData output = externalCommand.execute();
